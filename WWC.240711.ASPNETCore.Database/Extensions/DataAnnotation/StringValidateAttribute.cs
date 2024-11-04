d310f246de7131204deffe966affe2d499986ff9
Mynameaaa <2544025263@qq.com>
2024-11-04 21:57:45 +0800

    DataAnnotation 基本使用

---
+++ WWC.240711.ASPNETCore.Database/Extensions/DataAnnotation/StringValidateAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWC._240711.ASPNETCore.Database.Extensions.DataAnnotation
+{
+    /// <summary>
+    /// Long 类型验证
+    /// </summary>
+    public class StringValidateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is long longValue && longValue == 0)
+            {
+                return new ValidationResult($"{validationContext.DisplayName} cannot be 0.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
